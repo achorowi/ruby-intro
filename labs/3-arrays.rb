3805ac17dda6f948ec82d19b20af7acf7ec7b5eb
Amanda Horowitz <amanda.horowitz@kellogg.northwestern.edu>
2023-04-21 20:19:50 +0000

    updated for arrays

--- labs/3-arrays.rb
+++ labs/3-arrays.rb
@@ -12,6 +12,20 @@
 # If the two lists contain the same item, only show it once!
 # Lastly, display each item in the list prepended with "buy ".
 
+my_list = ["milk", "eggs", "bacon"]
+friend_list = ["beer", "cookies", "apples", "bacon"]
+combined_list = my_list + friend_list
+combined_list = combined_list.sort
+combined_list = combined_list.uniq  
+puts "buy #{combined_list[0]}"
+puts "buy #{combined_list[1]}"
+puts "buy #{combined_list[2]}"
+puts "buy #{combined_list[3]}"
+puts "buy #{combined_list[4]}"
+puts "buy #{combined_list[5]}"
+
+
+
 # HINTS
 # Learn to read the documentation!
 # https://ruby-doc.org/core-2.7.0/Array.html

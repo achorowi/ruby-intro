c07f42dc5bdd597b4ee806e059029db1facf948c
Amanda Horowitz <amanda.horowitz@kellogg.northwestern.edu>
2023-04-21 20:37:35 +0000

    hash commit

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,19 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+me = {
+    "name" => "Brian", 
+    "location" => {"city" => "Chicago", "state" => "Illinois"},
+    "timeline" => [
+        {"status" => "ENTR-451 at Kellogg", "posted" => "7:30am"},
+        {"status" => "Eating tacos", "posted" => "6:30am"}
+    ]
+    }
+
+#puts me.inspect
 
 # Accessing data from the hash
+puts me["timeline"]
+
 
 # More Complex Hashes
